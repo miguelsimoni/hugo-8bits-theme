dfc9ee6d191abf4cb852fef4d88762e8dcf689ce
Miguel.Simoni <miguel.simoni@gmail.com>
2021-05-25 11:58:29 -0300

    Add contact form.

---
+++ assets/js/contact.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector("form").addEventListener("submit", sendEmail);
+});
+
+function sendEmail(e) {
+  e.preventDefault();
+  e.stopPropagation();
+
+  var form = document.querySelector("form.needs-validation");
+  if (!form.checkValidity()) {
+    form.classList.add('was-validated');
+    return false;
+  }
+
+  document.querySelectorAll("button[type='submit'] > span").forEach(function (item) {
+    item.classList.toggle("visually-hidden");
+  });
+
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      document.querySelector("#contact").classList.add("visually-hidden");
+      document.querySelector("#thanks").classList.remove("visually-hidden");
+    }
+  };
+
+  xhttp.open("GET", document.querySelector("form").action, true);
+  xhttp.send();
+}
